983f385bd3b698f7fca910d26a05f905b44a8570
Gal <galnik.kozmelj@gmail.com>
2019-06-30 02:58:26 +0200

    frontend1

--- apostus/src/App.js
+++ apostus/src/App.js
@@ -6,9 +6,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img width="1080" src="img/apostus.png"></img>
+        <img width="1080" src="img/header2.png"></img>
       </header>
+      <body>
 
+        <div class="card">
+          <p>Release date?</p>
+
+
+          <div class="video_placeHolder">
+            <video width="960" height="540" controls>
+              <source src="img/test_level.mp4" type="video/mp4"></source>
+            </video>
+          </div>
+
+        </div>
+
+
+
+
+
+      </body>
     </div>
   );
 }
